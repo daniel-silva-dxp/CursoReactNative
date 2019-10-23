38efe9b68c7370c2f4d6600a6d3a744599a55bf4
Daniel Silva <dani3l.dxp@gmail.com>
2019-10-23 11:11:12 -0300

    Comitando primeiro projeto EXPO

--- PrimeiroExpoTeste/App.js
+++ PrimeiroExpoTeste/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá Mundo Daniel Silva</Text>
+      <Text style={styles.brandi}>DXPWeb Production</Text>
+      <Text style={styles.paragraph}>CRIADORES DE SITES DE CLASSE MUNDIAL</Text>
     </View>
   );
 }
@@ -12,8 +13,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00AC9E',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 8,
   },
+  brandi: {
+    margin: 15,
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#FFF45A'
+  },
+  paragraph: {
+    margin: 10,
+    fontSize: 10,
+    textAlign: 'center',
+    color: '#FFF'
+  }
+
 });
